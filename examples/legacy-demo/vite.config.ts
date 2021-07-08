ee56593cade4038c4cc3af3b6c6416edbcdc736b
Jy <1041207253@qq.com>
2021-07-09 00:32:52 +0800

    feat: add legacy browser demo

---
+++ examples/legacy-demo/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
+import { useDynamicPublicPath } from '../../dist/index'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    legacy({
+      targets: ['defaults', 'not ie <= 8']
+    }),
+    /** Attention! The legacy plugin must before loaded than dynamic-publicpath plugin */
+    useDynamicPublicPath({
+      dynamicImportHanlder: 'window.__dynamic_handler__',
+      dynamicImportPreload: 'window.__dynamic_preload__'
+    }),
+  ]
+})
