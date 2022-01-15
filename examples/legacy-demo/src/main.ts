6102847211e8386d95fae105e616080d18aa8c2f
Jy <1041207253@qq.com>
2022-01-15 19:08:48 +0800

    chore: update legacy-demo

--- examples/legacy-demo/src/main.ts
+++ examples/legacy-demo/src/main.ts
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+const dynamicCdn = ''; // Your cdn
+// @ts-ignore
+window.__dynamic_handler__ = function(importer) {
+    return dynamicCdn + importer;
+}
+// @ts-ignore
+window.__dynamic_preload__ = function(preloads: Array<string>) {
+    return preloads.map(preload => dynamicCdn + preload);
+}
+
 createApp(App).mount('#app')
